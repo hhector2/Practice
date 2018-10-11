d532b63791129bb3d07d134ecc40f5c3f09ad6b5
hector <hectorgiraldez@hotmail.com>
2018-10-11 02:29:56 +0100

    convective diffusive in cpp

---
+++ convective-diffusive.cpp
@@ -0,0 +1,15 @@
+//hello2
+
+//convective diffusive transport equation
+
+for (int i=0;i<maxX;i++) {
+	for (int j=0;j<maxY;j++) {
+	
+		u[i][j]=uo[i][j]+dif(neighbours[][])+conv(neighbours[i][j]);
+	}
+}
+
+double dif
+
+
+double conv
