672e59c5af3b9ad53b22e040245ff004f05d6685
hector <hectorgiraldez@hotmail.com>
2018-10-11 02:38:26 +0100

    convective diffusive in cpp draft

--- convective-diffusive.cpp
+++ convective-diffusive.cpp
@@ -1,15 +1,16 @@
-//hello2
-
 //convective diffusive transport equation
 
-for (int i=0;i<maxX;i++) {
-	for (int j=0;j<maxY;j++) {
+double dif(int i,int j){return 0.0;}
+double conv(int i,int j){return 0.0;}
+
+int main(){
+	int sizeX=100;
+	int sizeY=100;
+	double u[][],u[][];
+	for (int i=0;i<sizeX;i++) {
+		for (int j=0;j<sizeY;j++) {
 	
-		u[i][j]=uo[i][j]+dif(neighbours[][])+conv(neighbours[i][j]);
+			u[i][j]=uo[i][j]+dif(neighbours(i,j))+conv(neighbours(i,j));
 	}
 }
-
-double dif
-
-
-double conv
+}
