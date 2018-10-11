7e8dcaa1d264dbce49e3d44c09ad3c2ed75d6de8
hector <hectorgiraldez@hotmail.com>
2018-10-11 02:41:59 +0100

    convective diffusive in cpp compilable but empty functions

--- convective-diffusive.cpp
+++ convective-diffusive.cpp
@@ -6,11 +6,11 @@ double conv(int i,int j){return 0.0;}
 int main(){
 	int sizeX=100;
 	int sizeY=100;
-	double u[][],u[][];
+	double u[sizeX][sizeY],uo[sizeX][sizeY];
 	for (int i=0;i<sizeX;i++) {
 		for (int j=0;j<sizeY;j++) {
 	
-			u[i][j]=uo[i][j]+dif(neighbours(i,j))+conv(neighbours(i,j));
+			u[i][j]=uo[i][j]+dif(i,j)+conv(i,j);
 	}
 }
 }
